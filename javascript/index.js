fd738e2457dcf81248033a6441597e5512ec5622
Abiel Isak <abizmo99@gmail.com>
2023-01-31 03:42:25 +0100

    Iteration 3 using async/await

--- javascript/index.js
+++ javascript/index.js
@@ -119,7 +119,30 @@ obtainInstruction("steak", 0)
   });
 
 // Iteration 3 using async/await
-// ...
+const makeBroccoli = async()=>{
+  try {
+    const step1 = await obtainInstruction("broccoli", 0);
+    document.getElementById("broccoli").innerHTML += `<li>${step1}</li>`
+    const step2 = await obtainInstruction("broccoli", 1);
+    document.getElementById("broccoli").innerHTML += `<li>${step2}</li>`
+    const step3 = await obtainInstruction("broccoli", 2);
+    document.getElementById("broccoli").innerHTML += `<li>${step3}</li>`
+    const step4 = await obtainInstruction("broccoli", 3);
+    document.getElementById("broccoli").innerHTML += `<li>${step4}</li>`
+    const step5 = await obtainInstruction("broccoli", 4);
+    document.getElementById("broccoli").innerHTML += `<li>${step5}</li>`
+    const step6 = await obtainInstruction("broccoli", 5);
+    document.getElementById("broccoli").innerHTML += `<li>${step6}</li>`
+    const step7 = await obtainInstruction("broccoli", 6);
+    document.getElementById("broccoli").innerHTML += `<li>${step7}</li>`
+    document.getElementById("broccoli").innerHTML += "<li>Broccoli is ready!</li>"
+   
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
